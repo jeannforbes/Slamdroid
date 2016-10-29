bde2c9f2be38ff7157459e9e2d14edf547389b70
jeannforbes <jeannette.forbes@gmail.com>
2016-10-28 23:23:36 -0400

    Slam concept implementation
    Spacebar to slam, gotta android jam

---
+++ Slamdroid/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	private GameObject Player;
+	private Player playerScript;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		playerScript = Player.GetComponent ("Player") as Player;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerStay2D(Collider2D other) {
+		if (other.CompareTag ("Player")) {
+			Player.GetComponent<Rigidbody2D>().AddForce(playerScript.Vel * -20);
+		}
+	}
+
+}
---
+++ Slamdroid/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Vector3 accel, vel;
+	public float friction = 0.1f;
+
+	public Vector3 Accel{
+		get{ return accel; }
+		set{ accel = value; }
+	}
+
+	public Vector3 Vel{
+		get{ return vel; }
+		set{ vel = value; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		accel = new Vector3 ();
+		vel = new Vector3 ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			accel += Vector3.right * Time.deltaTime * 1000;
+		}
+		if(vel.x > 0) vel.x -= friction;
+		if (vel.x < 0) vel.x += friction;
+
+		vel += accel;
+		GetComponent<Rigidbody2D> ().AddForce (vel);
+		accel = Vector3.zero;
+	}
+}
