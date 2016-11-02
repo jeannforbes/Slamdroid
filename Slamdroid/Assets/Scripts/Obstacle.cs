0eb8159921a7fa9ec14b80f32ee52f54880b3a88
jeannforbes <jeannette.forbes@gmail.com>
2016-11-01 21:35:46 -0400

    Preliminary obstacle script
    Obstacles fall out of the world after being collided with, but slow down
    the player too much.

---
+++ Slamdroid/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+	private GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject == Player) {
+			this.GetComponent<Collider2D>().enabled = false;
+		}
+	}
+
+
+}
