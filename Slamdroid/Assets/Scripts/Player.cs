d44babb47f3f43a382b7f9722e45491497b08a50
BRIAN SANDON (RIT Student) <bxs9775@ad.rit.edu>
2016-11-03 17:05:35 -0400

    Added launch/boost sequence and gave it a sprite * the current sprite is a placeholder * we need some way to deal with velocity hitting 0, reset player

--- Slamdroid/Assets/Scripts/Player.cs
+++ Slamdroid/Assets/Scripts/Player.cs
@@ -4,11 +4,19 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	private GameObject cam;
-	private bool canAccel = true;
+    private bool canAccel = true;
+
 	private Vector2 accel;
 	public float friction = 10f;
 
-	public Vector2 Accel{
+    //Starting boost
+    private float boostTimer = 0;
+    private float boostFactor = 0;
+    private const float MAX_ACCEL = 10000f;
+    public GameObject boostLine;
+    public GameObject boostBar;
+
+    public Vector2 Accel{
 		get{ return accel; }
 		set{ accel = value; }
 	}
@@ -20,21 +28,54 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//Player input
-		if (canAccel && Input.GetKeyDown (KeyCode.Space)) {
+        //Player input
+        /*if (canAccel && Input.GetKeyDown (KeyCode.Space)) {
 			accel += Vector2.right * Time.deltaTime * 2000;
 			//canAccel = false;
-		}
+		}*/
+        if (canAccel)
+        {
+            boostTimer += Time.deltaTime;
+            boostFactor = -Mathf.Cos(boostTimer*5);
 
-		GetComponent<Rigidbody2D>().velocity += accel;
+            Vector3 barPosition = 1.5f * Vector2.up * boostFactor;
+            barPosition.z = -0.25f;
+
+            boostLine.transform.localPosition = barPosition;
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                accel += Vector2.right * Time.deltaTime * MAX_ACCEL * (1 - Mathf.Abs(boostFactor));
+                canAccel = false;
+
+                boostBar.GetComponent<SpriteRenderer>().enabled = false;
+                boostLine.GetComponent<SpriteRenderer>().enabled = false;
+            }
+        }
+
+        GetComponent<Rigidbody2D>().velocity += accel;
 		accel = Vector2.zero;
 
-		//Update camera position
-		cam.transform.position = new Vector3(transform.position.x, transform.position.y, -35);
+        /*We need some way to deal with the player hitting 0 velocity.
+         * 
+        if (!canAccel && GetComponent<Rigidbody2D>().velocity == Vector2.zero)
+        {
+            Reset();
+        }
+        */
+
+        //Update camera position
+        cam.transform.position = new Vector3(transform.position.x, transform.position.y, -35);
+
+
 	}
 
 	public void Reset(){
 		transform.position = Vector2.zero;
 		canAccel = true;
-	}
+        boostTimer = 0;
+
+        boostBar.GetComponent<SpriteRenderer>().enabled = true;
+        boostLine.GetComponent<SpriteRenderer>().enabled = true;
+    }
 }
