6d4bb22c053093b0fff83854f67c6d2bfd294a73
BRIAN SANDON (RIT Student) <bxs9775@g.rit.edu>
2016-11-06 17:13:38 -0500

    Started work on the State Machine

--- Slamdroid/Assets/Scripts/Player.cs
+++ Slamdroid/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	private GameObject cam;
-    private bool canAccel = true;
+    //private bool canAccel = true;
 
 	private Vector2 accel;
 	public float friction = 10f;
@@ -15,69 +15,89 @@ public class Player : MonoBehaviour {
     private const float MAX_ACCEL = 10000f;
     private GameObject boostLine;
 	private GameObject boostBar;
+    
+    //player's state
+    public enum PlayerState
+    {
+        Ready, Running, Stopped
+    }
+    private PlayerState playState;
 
     public Vector2 Accel{
 		get{ return accel; }
 		set{ accel = value; }
 	}
 
+    public PlayerState PlayState
+    {
+        get { return playState; }
+    }
+
 	void Start(){
 		cam = GameObject.FindGameObjectWithTag ("MainCamera");
 		boostBar = GameObject.FindGameObjectWithTag ("BoostBar");
 		boostLine = boostBar.transform.FindChild ("BoostLine").gameObject;
 		GetComponent<Rigidbody2D> ().freezeRotation = true;
-	}
+
+        playState = PlayerState.Stopped;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        //Player input
-        /*if (canAccel && Input.GetKeyDown (KeyCode.Space)) {
-			accel += Vector2.right * Time.deltaTime * 2000;
-			//canAccel = false;
-		}*/
-        if (canAccel)
+        //print("Player State: " + playerState + "\n");
+        switch (playState)
         {
-            boostTimer += Time.deltaTime;
-            boostFactor = -Mathf.Cos(boostTimer*5);
+            case PlayerState.Ready:
+                boostTimer += Time.deltaTime;
+                boostFactor = -Mathf.Cos(boostTimer * 5);
 
-            Vector3 barPosition = 1.5f * Vector2.up * boostFactor;
-            barPosition.z = -0.25f;
+                Vector3 barPosition = 1.5f * Vector2.up * boostFactor;
+                barPosition.z = -0.3f;
 
-            boostLine.transform.localPosition = barPosition;
+                boostLine.transform.localPosition = barPosition;
 
-            if(Input.GetKeyDown(KeyCode.Space))
-            {
-                accel += Vector2.right * Time.deltaTime * MAX_ACCEL * (1 - Mathf.Abs(boostFactor));
-                canAccel = false;
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    accel += Vector2.right * Time.deltaTime * MAX_ACCEL * (1 - Mathf.Abs(boostFactor));
+                    //canAccel = false;
+                    playState = PlayerState.Running;
 
-                boostBar.GetComponent<SpriteRenderer>().enabled = false;
-                boostLine.GetComponent<SpriteRenderer>().enabled = false;
-            }
+                    boostBar.GetComponent<SpriteRenderer>().enabled = false;
+                    boostLine.GetComponent<SpriteRenderer>().enabled = false;
+                }
+                break;
+            case PlayerState.Running:
+                GetComponent<Rigidbody2D>().velocity += accel;
+                accel = Vector2.zero;
+
+                //We need some way to deal with the player hitting 0 velocity.
+
+                if (GetComponent<Rigidbody2D>().velocity == Vector2.zero)
+                {
+                    playState = PlayerState.Stopped;
+                }
+                break;
+            case PlayerState.Stopped:
+                Reset();
+                break;
         }
-
-        GetComponent<Rigidbody2D>().velocity += accel;
-		accel = Vector2.zero;
-
-        //We need some way to deal with the player hitting 0 velocity.
-
-        if (!canAccel && GetComponent<Rigidbody2D>().velocity == Vector2.zero)
-        {
-            Reset();
-        }
-
 
         //Update camera position
         cam.transform.position = new Vector3(transform.position.x, transform.position.y, -35);
-
-
-	}
+    }
 
 	public void Reset(){
 		transform.position = Vector2.zero;
-		canAccel = true;
+        //canAccel = true;
+        playState = PlayerState.Ready;
         boostTimer = 0;
 
         boostBar.GetComponent<SpriteRenderer>().enabled = true;
         boostLine.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    void OnEnable()
+    {
+        playState = PlayerState.Ready;
     }
 }
---
+++ Slamdroid/Assets/Scripts/StateManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateManager : MonoBehaviour {
+    public enum GameState
+    {
+        MainMenu,Play,Pause,Instructions,Upgrade
+    }
+
+    private GameObject player;
+    private Player playerScript;
+
+    private GameState state;
+    private Rect menuRect;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<Player>();
+
+        changeState(GameState.MainMenu);
+        menuRect = new Rect(0.0f, 0.0f, 150f, 100f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch (state)
+        {
+            case GameState.MainMenu:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    changeState(GameState.Play);
+                }
+                break;
+            case GameState.Play:
+                if (Input.GetKeyDown(KeyCode.P) && playerScript.PlayState != Player.PlayerState.Running)
+                {
+                    changeState(GameState.Pause);
+                }
+                break;
+            case GameState.Pause:
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    changeState(GameState.Play);
+                }
+                break;
+        }
+    }
+
+    void OnGUI()
+    {
+        switch (state)
+        {
+            case GameState.MainMenu:
+                GUI.TextArea(menuRect, "Slamdroid\n\n(Press space to play)");
+                break;
+            case GameState.Pause:
+                GUI.TextArea(menuRect, "The game is paused.\n(Press P to unpause)");
+                break;
+        }
+    }
+
+    public void changeState(GameState newState)
+    {
+        if (newState == GameState.Play)
+        {
+            playerScript.enabled = true;
+            player.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            playerScript.enabled = false;
+            if (newState != GameState.Pause)
+            {
+                player.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+            }
+        }
+        state = newState;
+    }
+}
